b8609b2691f67860d71950cba51156a58a912677
wentian-pixent <wentian.lee@pixent.net>
2025-08-11 02:34:08 +0000

    fix incorrect port number

--- cron.sh
+++ cron.sh
@@ -2,4 +2,4 @@
 folder="/mongodump-output"
 fileName="$(date '+%Y-%m-%d-%H-%M-%S').log"
 mkdir -p "$folder"
-mongodump --uri="mongodb://host.docker.internal:27107" --db=playground --collection=providers --out=/var/backups >> "$folder/$fileName" 2>&1
+mongodump --uri="mongodb://host.docker.internal:27017" --db=playground --collection=providers --out=/var/backups >> "$folder/$fileName" 2>&1
